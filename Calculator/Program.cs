0415865deca34842a6d79bfd88f2c00c1dd91f18
BrendonAraujo <brendong.araujo@gmail.com>
2023-01-07 10:48:37 -0300

    #ADD - Calculator sum and substration functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculator;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Soma();
+		Subtracao();
+	}
+
+	static void Soma()
+	{
+		Console.Clear();
+		Console.WriteLine("Primeiro Valor");
+		float primeiroValor = float.Parse(Console.ReadLine());
+
+		Console.WriteLine("Segundo valor");
+		float segundoValor = float.Parse(Console.ReadLine());
+
+		float soma = primeiroValor + segundoValor;
+
+		Console.WriteLine("");
+		Console.WriteLine($"O valor da soma é {soma}");   
+	}
+	
+	static void Subtracao()
+	{
+		Console.Clear();
+		Console.WriteLine("Primeiro Valor");
+		float primeiroValor = float.Parse(Console.ReadLine());
+
+		Console.WriteLine("Segundo valor");
+		float segundoValor = float.Parse(Console.ReadLine());
+
+		float soma = primeiroValor - segundoValor;
+
+		Console.WriteLine("");
+		Console.WriteLine($"O valor da subtracao é {soma}");   
+	}
+
+
+}
